ff8813255b9612353094172c8b7f15ae10f925d6
Paulo <paulo.braguim@outlook.com>
2021-10-28 17:15:07 -0300

    Ajuste backend

--- recebeDados.php
+++ recebeDados.php
@@ -2,17 +2,17 @@
 
 require('app/Classes/Amortizacao.php');
 
-$saldoDevedor = $_POST['saldoDevedor'];
-$taxa = $_POST['taxa'];
-$parcelas = $_POST['parcelas'];
-$tipo = $_POST['tipo'];
-
 if(!isset($_POST['saldoDevedor']) || !isset($_POST['taxa']) || !isset($_POST['parcelas']) || !isset($_POST['tipo'])){
     
     header('Location: /');
     exit;
 
 }else {
+
+    $saldoDevedor = $_POST['saldoDevedor'];
+    $taxa = $_POST['taxa'];
+    $parcelas = $_POST['parcelas'];
+    $tipo = $_POST['tipo'];
 
     $var = new Amortizacao($saldoDevedor, $parcelas, $taxa);
 
